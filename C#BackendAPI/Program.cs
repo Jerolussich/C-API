b0f90d9d6ac150cb75de8af22ab1e09ee12448f8
Jeronimo Lussich <jerolussich@hotmail.com>
2023-04-19 02:45:38 -0300

    Backend API added

---
+++ C#BackendAPI/Program.cs
@@ -0,0 +1,37 @@
+global using C_BackendAPI.Models;
+global using C_BackendAPI.Services.CharacterService;
+global using C_BackendAPI.Dtos.Character;
+global using C_BackendAPI.Data;
+global using AutoMapper;
+global using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
+builder.Services.AddScoped<ICharacterService, CharacterService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
